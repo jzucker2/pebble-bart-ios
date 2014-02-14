a87eea33ee6fc0a55853bc0dd41fc054de3275be
Jordan Zucker <jordan.zucker@gmail.com>
2014-02-14 09:54:55 -0800

    Can parse xml from bart, need to create base objects

--- PebbleBart/PebbleBart/BartAPI.m
+++ PebbleBart/PebbleBart/BartAPI.m
@@ -8,6 +8,7 @@
 
 #import "BartAPI.h"
 #import <AFNetworking.h>
+#import <TouchXML.h>
 
 @implementation BartAPI
 
@@ -24,9 +25,21 @@
 {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFXMLParserResponseSerializer new];
+    //manager.responseSerializer = [AFHTTPResponseSerializer new];
     [manager GET:@"http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"XML: %@", responseObject);
-        NSXMLParser *parser = responseObject;
+//        NSLog(@"XML: %@", responseObject);
+//        NSXMLParser *parser = responseObject;
+        NSData * data = operation.responseData;
+        //NSString *xmlString = [NSString stringWithUTF8String:[data bytes]];
+        NSString *xmlString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"-------------------------------");
+        //NSLog(@"Response string: %@", xmlString);
+        //NSLog(@"+++++++++++++++++++++++++++++++");
+        CXMLDocument *doc = [[CXMLDocument alloc] initWithXMLString:xmlString options:0 error:nil];
+        NSArray *stations = [doc nodesForXPath:@"//stations/station" error:nil];
+        NSLog(@"stations is %@", stations);
+        NSLog(@"-------------------------------");
+        
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
--- Podfile
+++ Podfile
@@ -1,4 +1,6 @@
 platform:ios, '7.0'
 
 xcodeproj 'PebbleBart/PebbleBart.xcodeproj'
+
 pod "AFNetworking", "~> 2.0"
+pod "TouchXML", "~> 0.1"
